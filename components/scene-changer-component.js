2cc900c698f1fe5dc3da20575ce4f33701c233f8
RocketLL <luc4sl33@gmail.com>
2019-02-14 04:29:19 +0900

    welcome screen and video

--- components/scene-changer-component.js
+++ components/scene-changer-component.js
@@ -1,19 +1,18 @@
 AFRAME.registerComponent("scene-changer", {
   schema: {
     from: { type: "string", default: "welcome" },
-    to: { type: "string", default: "joseon" }
+    to: { type: "string", default: "menu" }
   },
 
   init: function() {
     var data = this.data;
     var el = this.el;
+    console.log(this.data);
     this.clickHandler = function() {
       var from = "scene-" + data.from;
       var to = "scene-" + data.to;
       var sceneFrom = document.getElementById(from);
       var sceneTo = document.getElementById(to);
-      sceneFrom.setAttribute("visible", "false");
-      sceneTo.setAttribute("visible", "true");
 
       var childrenFrom = sceneFrom.children;
       var childrenTo = sceneTo.children;
@@ -25,8 +24,11 @@ AFRAME.registerComponent("scene-changer", {
       for (i = 0; i < childrenTo.length; i++) {
         childrenTo[i].setAttribute("visible", "true");
       }
-
+      sceneFrom.setAttribute("visible", "false");
+      sceneTo.setAttribute("visible", "true");
+        
       el.removeEventListener("click", this.clickHandler);
+      console.log(from + " to " + to);
     };
     el.addEventListener("click", this.clickHandler);
   }
